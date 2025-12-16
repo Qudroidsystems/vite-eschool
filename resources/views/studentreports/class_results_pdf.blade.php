dfb6494bf83488415bf69185019e6874687de4d4
Eliab <35658748+eliabsiji@users.noreply.github.com>
2025-12-16 22:02:46 +0100

    Update class_results_pdf.blade.php

--- resources/views/studentreports/class_results_pdf.blade.php
+++ resources/views/studentreports/class_results_pdf.blade.php
@@ -789,7 +789,7 @@
                         <tr>
                             <td>
                                 <span class="font-bold">This Result was issued on  </span>
-                                <span class="text-dot-space2"> 11th August, 2025</span>
+                                <span class="text-dot-space2"> 18th December, 2025</span>
                         
                                 <span class="font-bold">and collected by</span>
                                 <span class="">.......................................</span>
